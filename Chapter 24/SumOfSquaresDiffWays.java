03e46fce8265a0be6b5482ec4102bba53cc43afa
Vince Jevy Tapdasan <143710577+Jebbxrs@users.noreply.github.com>
2024-03-18 23:06:00 +0800

    Update SumOfSquaresDiffWays.java

--- Chapter 24/SumOfSquaresDiffWays.java
+++ Chapter 24/SumOfSquaresDiffWays.java
@@ -1,28 +1,33 @@
 import java.util.Scanner;
 public class SumOfSquaresDiffWays {
     public static void main(String[] args) {
-        int input, instance = 0;
+        int input, instance = 0, k_temp = 0, j_temp = 0;
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Enter a positive integer: ");
         input = scan.nextInt();
 
-        for (int i = 0; Math.pow(i, 2) < input; i++) {
-            for (int j = 0; Math.pow(j, 2) + Math.pow(i, 2) <= input; j++) {
-                if (Math.pow(j, 2) + Math.pow(i, 2) == input && instance == 0){
-                    System.out.println(input + " is a sum of two squares");
-                    System.out.println("Instance " + (instance + 1) + ": " + input + " = " + i + "*" + i + " + " + j + "*" + j);
+        for (int j = (int) Math.sqrt(input / 2); j * j < input; j++) {
+            int k = (int) Math.sqrt(input - j * j);
+            if (j * j + k * k == input) {
+                instance++;
+
+                if (instance == 1) {
+                    j_temp = j;
+                    k_temp = k;
+                }
+                else if (instance == 2) {
+                    System.out.println(input + " is the sum of 2 squares in two different ways");
+                    System.out.println(input + " == " + j_temp + "*" + j_temp + " + " + k_temp + "*" + k_temp);
+                    System.out.println(input + " == " + j + "*" + j + " + " + k + "*" + k);
                     instance++;
                 }
-                else if (Math.pow(j, 2) + Math.pow(i, 2) == input && instance > 0) {
-                    System.out.println("Instance " + (instance + 1) + ": " + input + " = " + i + "*" + i + " + " + j + "*" + j);
-                    instance++;
-                }
+                else if (instance > 2)
+                    System.out.println(input + " == " + j + "*" + j + " + " + k + "*" + k);
             }
-            
         }
-        if (instance == 0)
-            System.out.println("It is not a sum of two squares");
+        if (instance < 2) 
+            System.out.println(input + " is not a sum of two positive integers in two different ways.");
         scan.close();
     }
 }
