2c83d97a1f20d45814dbd96bebda353d48b67d83
Vince Jevy Tapdasan <143710577+Jebbxrs@users.noreply.github.com>
2024-03-18 20:46:22 +0800

    Add files via upload

--- Chapter 24/MaxIntOfFile.java
+++ Chapter 24/MaxIntOfFile.java
@@ -1,9 +1,14 @@
+/*This program reads the first 5 integers in a given file and prints out the maximum value and sum of the integers.
+ * Written by: Vince Jevy Tapdasan
+ * Date: March 15, 2024;
+ */
 import java.util.Scanner;
+
 public class MaxIntOfFile {
     public static void main(String[] args) {
         int max = 0, fileInput, numCount = 0, sum = 0;
         Scanner scan = new Scanner(System.in);
-
+ 
         while (numCount < 5) {
             fileInput = scan.nextInt();
 
